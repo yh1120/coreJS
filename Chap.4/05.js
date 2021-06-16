35843630022d8c9c4a8498a9011fe047981baa31
yonglee <yonglee@student.42seoul.kr>
2021-06-16 20:47:01 +0900

    Chap.4_05

--- Chap.4/05.js
+++ Chap.4/05.js
@@ -1,10 +1,10 @@
 // 05가 잘 이해가 안됨
 
 Array.prototype.map = function (callback, thisArg) {
-    var mappedArr = [];
-    for (var i = 0; i < this; i++) {
-        var mappedValue = callback.call(thisArg || window, this[i], i, this);
-        mappedArr[i] = mappedValue;
-    }
-    return mappedArr;
-}
+  var mappedArr = [];
+  for (var i = 0; i < this; i++) {
+    var mappedValue = callback.call(thisArg || global, this[i], i, this);
+    mappedArr[i] = mappedValue;
+  }
+  return mappedArr;
+};
